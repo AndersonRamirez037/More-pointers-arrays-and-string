687d927df025c73ac56b5845d834fa3cbac38454
AndersonRamirez037 <andersongamer037@gmail.com>
2023-04-28 08:37:13 -0500

    0. strcat

--- 0-strcat.c
+++ 0-strcat.c
@@ -1,10 +1,19 @@
 #include "main.h"
 #include <string.h>
+#include <stdio.h>
 /*
- *
- *
- *
+ * _strcat - function that concatenates two strings
+ * @char
+ * return char.
 */
 char *_strcat(char *dest, char *src){
-    return strcat(dest,src + '\0');
+    int size = 0,i;
+    while (dest[size]){
+        size++;
+    }
+    for(i = 0; src[i] != '\0';i++){
+        dest[size] = src[i];
+        size++;
+    }
+    return dest;
 }
